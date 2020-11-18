82673c0208e81f0c3a88ccdbc6375a0a14f4b81c
stefian <v_ianta@yahoo.com>
2020-11-18 13:13:08 -0400

    +     const redis = require('redis');     const redisUrl = 'redis://127.0.0.1:6379';     const client = redis.createClient(redisUrl);

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,7 +15,8 @@ module.exports = app => {
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
     const redis = require('redis');
-    const redisURL = 'redis://127.0.0.1:6379';
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
 
     const blogs = await Blog.find({ _user: req.user.id });
 
