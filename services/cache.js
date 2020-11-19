deadeea3897bb0a3114b7c4ace7902fb3041a720
stefian <v_ianta@yahoo.com>
2020-11-19 16:24:47 -0400

    + // Overwriting the Query.exec() to include caching with Redis // mongoose.Query.prototype.exec = function () {
    }

--- services/cache.js
+++ services/cache.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
 const exec = mongoose.Query.prototype.exec;
+
+// Overwriting the Query.exec() to include caching with Redis //
+mongoose.Query.prototype.exec = function () {
+
+}
