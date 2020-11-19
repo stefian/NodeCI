0fdb4a1371e0c1eab8b0afc1df5aa4925f81ea83
stefian <v_ianta@yahoo.com>
2020-11-19 16:29:17 -0400

    + console.log('IM ABOUT TO RUN A QUERY'); // To confirm we can hook into/pre exec() call //

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,7 @@ const exec = mongoose.Query.prototype.exec;
 
 // Overwriting the Query.exec() to include caching with Redis //
 mongoose.Query.prototype.exec = function () {
+  console.log('IM ABOUT TO RUN A QUERY'); // To confirm we can hook into/pre exec() call //
 
+  return exec.apply(this, arguments); // The original Mongoose exec() code // 
 }
