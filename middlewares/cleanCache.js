c5040fcb08b9ffbe243a889a8b288049d33bf28c
stefian <v_ianta@yahoo.com>
2020-11-21 18:48:45 -0400

    + const { clearHash } = require('../services/cache');
    module.exports = async (req, res, next) => {
      await next(); // execute the function after the route handler //
    
      clearHash(req.user.id);
    };

--- middlewares/cleanCache.js
+++ middlewares/cleanCache.js
@@ -1,7 +1,7 @@
-const { clearCache } = require('../services/cache');
+const { clearHash } = require('../services/cache');
 
 module.exports = async (req, res, next) => {
   await next(); // execute the function after the route handler //
-  
 
+  clearHash(req.user.id);
 };
