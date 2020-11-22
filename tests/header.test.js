247704d693e156656b85d30e1808d1f3d4f700e6
stefian <v_ianta@yahoo.com>
2020-11-22 17:05:14 -0400

    + // jest.setTimeout(30000); //

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,12 +1,7 @@
 const puppeteer = require('puppeteer');
 
-jest.setTimeout(30000);
+// jest.setTimeout(30000); //
 
-test('Adds 2 numbers', () => {
-  const sum = 1 + 2;  
-
-  expect(sum).toEqual(3);
-});
 
 test('We can launch a browser', async () => {
   const browser = await puppeteer.launch({
