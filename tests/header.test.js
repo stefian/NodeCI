dd8b632b72fcab1ea86b2104a926218fada3acd1
stefian <v_ianta@yahoo.com>
2020-11-25 16:27:28 -0400

    + const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');

--- tests/header.test.js
+++ tests/header.test.js
@@ -39,5 +39,6 @@ test('When signed in, shows logout button', async () => {
       user: id
     }
   };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
 
 });
