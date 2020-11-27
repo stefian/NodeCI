6b80ca38c47378be02fc9ab0b155cb47ea16112e
stefian <v_ianta@yahoo.com>
2020-11-27 14:22:16 -0400

    + headless: true

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -7,7 +7,7 @@ const userFactory = require('../factories/userFactory');
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true
     });
 
     const page = await browser.newPage();
