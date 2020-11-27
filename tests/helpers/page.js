676acee9b121a03aa59d92fee32c3bb31adf96a9
stefian <v_ianta@yahoo.com>
2020-11-27 14:20:18 -0400

    +   async login() {     const user = await userFactory(); // Create new user for the test //     const { session, sig } = sessionFactory(user);
        await this.page.setCookie({ name: 'session', value: session, domain: 'localhost:3000' });
        await this.page.setCookie({ name: 'session.sig', value: sig, domain: 'localhost:3000' });
        await this.page.reload({ waitUntil: 'domcontentloaded' });
        await this.page.waitForSelector('a[href="/auth/logout"]'); // tests might fail here instead of expectation statement //
      }

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
 
 // Additional to calss Page from Puppeteer //
 // for W3AI - to extend as class **recombinant Repo** //
@@ -20,6 +22,16 @@ class CustomPage {
 
   constructor(page) {
     this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory(); // Create new user for the test //
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: 'session', value: session, domain: 'localhost:3000' });
+    await this.page.setCookie({ name: 'session.sig', value: sig, domain: 'localhost:3000' });
+    await this.page.reload({ waitUntil: 'domcontentloaded' });
+    await this.page.waitForSelector('a[href="/auth/logout"]'); // tests might fail here instead of expectation statement //
   }
 }
 
