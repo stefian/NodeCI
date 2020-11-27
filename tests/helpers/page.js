b166b67a519f2931f3dde3226b5cc4e3f3af6b7e
stefian <v_ianta@yahoo.com>
2020-11-27 12:16:29 -0400

    + // Additional to calss Page from Puppeteer // // for W3AI - to extend as class **recombinant Repo** // class CustomPage {

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,5 +1,26 @@
 const puppeteer = require('puppeteer');
 
+// Additional to calss Page from Puppeteer //
+// for W3AI - to extend as class **recombinant Repo** //
 class CustomPage {
-  
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return customPage[property] || page[property] || browser[property];
+      }
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
 }
+
+module.exports = CustomPage;
