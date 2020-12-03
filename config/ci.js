b4223011f9068a5d3d6c37f5ed2b5e1ff7f7c38b
stefian <v_ianta@yahoo.com>
2020-12-03 10:19:23 -0400

    + dev.js

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: 'mongodb://127.0.0.1/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
