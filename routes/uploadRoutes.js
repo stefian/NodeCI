756ca5d65344803f3ee5a6afa5cc8d0c4cdd3bad
stefian <v_ianta@yahoo.com>
2020-12-06 11:45:43 -0400

    + const s3 = new AWS.S3({   accessKeyId: keys.accessKeyId,   secretAccessKey: keys.secretAccessKey });

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,6 +1,11 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
 
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey
+});
+
 module.exports = app => {
   app.get('/api/upload', (req, res) => {
 
