4090393775b603c0db09bc4f049dad14d28f541a
stefian <v_ianta@yahoo.com>
2020-12-06 17:53:18 -0400

    +     s3.getSignedUrl('putObject', {       Bucket: 'ai-blog-bucket',       ContentType: 'jpg',       Key: key     }, (err, url) => res.send({ key, url }));

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
 const s3 = new AWS.S3({
@@ -7,7 +9,13 @@ const s3 = new AWS.S3({
 });
 
 module.exports = app => {
-  app.get('/api/upload', (req, res) => {
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid}.jpg`;
 
+    s3.getSignedUrl('putObject', {
+      Bucket: 'ai-blog-bucket',
+      ContentType: 'jpg',
+      Key: key
+    }, (err, url) => res.send({ key, url }));
   });
 };
